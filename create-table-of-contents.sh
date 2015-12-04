e45a5d86defe03e62c089a94f36cbfa10d918c60
Mr. Is <isaac.ardis@gmail.com>
2015-12-04 08:31:41 -0500

    table of contents -> readme

---
+++ create-table-of-contents.sh
@@ -0,0 +1,14 @@
+# Load from dotfiles.
+# do like holman
+export ZSH=$HOME/.dotfiles
+config_files=$ZSH/**/*.zsh
+
+## except load everything
+for file in $config_files
+do
+				echo "\`\`\`bash" >> README.md
+        echo "$(cat $file)" >> README.md
+        echo "\`\`\`" >> README.md
+done
+
+unset config_files
