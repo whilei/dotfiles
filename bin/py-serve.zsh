d37eefb9708f1d77ea27ca3577af939a6a4a916b
Mr. Is <isaac.ardis@gmail.com>
2015-12-10 10:34:14 -0500

    py server

---
+++ bin/py-serve.zsh
@@ -0,0 +1,24 @@
+# Open up a terminal and type:
+
+# $ cd /home/somedir
+# $ python -m SimpleHTTPServer
+# That's it! Now your http server will start in port 8000. You will get the message:
+
+# Serving HTTP on 0.0.0.0 port 8000 ...
+# Now open a browser and type the following address:
+
+# http://192.168.1.2:8000
+# You can also access it via:
+
+# http://127.0.0.1:8000
+# If the directory has a file named index.html, that file will be served as the initial file. If there is no index.html, then the files in the directory will be listed.
+
+# If you wish to change the port that's used start the program via:
+
+# $ python -m SimpleHTTPServer 8080
+
+#!/bin/bash 
+
+pyserv(){
+	python -m SimpleHTTPServer $1
+}
