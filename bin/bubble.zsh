e00702eb44c537777b29c48fca91a312a7de0e48
Mr. Is <isaac.ardis@gmail.com>
2015-12-23 09:33:17 -0500

    add contact

--- bin/bubble.zsh
+++ bin/bubble.zsh
@@ -1,6 +1,15 @@
+export contact_sheet=$HOME/.dotfiles/private/contacts.sh
+
 get_contacts(){
-	. ~/.dotfiles/private/contacts.sh
+	. $contact_sheet
 	echo Got contacts. 
+}
+
+add_contact(){
+	local name="$1m"
+	echo "export $name"\="$2" >> $contact_sheet
+	. $contact_sheet
+	echo Added "$name" @ "$2" to contacts. 
 }
 
 msg(){
