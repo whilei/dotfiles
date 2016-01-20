9bc26c4a20a85b6cd7e8cd06c74934f7bc0c16d9
Mr. Is <isaac.ardis@gmail.com>
2016-01-20 07:20:26 -0500

    git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,18 @@
 alias gg='git grep'
+
+alias ga="git add"
 alias gitit='git add -A && git commit -m'
+
 alias gs='git status -sb'
 alias gl="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gll="git log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an$"
+
 alias gpo="git push origin"
+alias gpullo="git pull origin"
+alias gf="git fetch"
+
 alias gco="git checkout"
+alias gcb="git checkout -b"
+
+alias greb="git rebase"
+
