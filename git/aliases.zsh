f03f21353e7836ad5fb006b9df0ed5d96849e4a5
Mr. Is <isaac.ardis@gmail.com>
2016-02-12 15:39:35 -0500

    reckoning git aliases and configgers

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -5,7 +5,9 @@ alias gcm="git commit -m"
 alias gitit='git add -A && git commit -m'
 
 alias gs='git status -sb'
-alias gl='git log --oneline --decorate --color --graph'
+alias gll='git --no-pager log --oneline --decorate --color --graph'
+# Accepts -<3> arg to limit number shown. 
+alias glg='git --no-pager log --pretty=format:"%cr [%h] %s" --graph --stat'
 
 alias gpo="git push origin"
 alias gpullo="git pull origin"
