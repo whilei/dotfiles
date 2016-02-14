bf7b2bdc30726decda250fa7b3135bf94715a1a5
Mr. Is <isaac.ardis@gmail.com>
2016-02-13 20:14:02 -0500

    toph -> /dev/null

--- system/tophify.zsh
+++ system/tophify.zsh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # https://github.com/tophtucker
 if ! [ $(nc -z localhost 8000) ]; then 
-	pushd /Users/ia/dev/tophtweets-master; python -m SimpleHTTPServer; popd
+	pushd /Users/ia/dev/tophtweets-master; python -m SimpleHTTPServer &>/dev/null; popd
 fi
