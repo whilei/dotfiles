13638131446924b99785ef52a66a8d30c293e759
Mr. Is <isaac.ardis@gmail.com>
2016-02-18 12:13:19 -0500

    eachdir from mr cowbody

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,9 +3,10 @@ alias psg="ps aux | grep -v grep | grep -i -e VSZ -e"
 
 # make dir and cd into it
 mkcd(){
-	mkdir -pv $1
-	cd $1
+	 mkdir -p "$@" && cd "$@" # thanks cowboy. this is better
 }
+
+alias eachdir=". ~/.dotfiles/bashers/each-dir.zsh"
 
 # The -a flag is for archive, which "ensures that symbolic links, devices, attributes, permissions, ownerships, etc. are preserved in the transfer"; the -z flag compresses files during the transfer; -v is for verbose; and --progress shows you your progress. I've enshrined this in an alias:
 # Copy the files pointed to by the symbolic links ("transform symlink into referent file/dir") with the --L flag:
