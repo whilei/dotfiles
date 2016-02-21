c7f48f51a6d5ad79613c200d9d6e2d88115758d1
Mr. Is <isaac.ardis@gmail.com>
2016-02-21 13:41:15 -0500

    note

--- funk/aliases.zsh
+++ funk/aliases.zsh
@@ -1,4 +1,4 @@
 # Smoke the hash.
 alias busy="cat /dev/urandom | hexdump -C | grep 'ca fe'" 
 alias vid2gif=". ~/.dotfiles/funk/video-to-gif.sh"
-alias paper="bash ./paper.sh"
+alias paper="bash ./paper.sh" # explicit bash command here seems to fix ? some getopts issues
