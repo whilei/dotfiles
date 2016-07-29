a3e470e304371c80001c582b12f080d29aabc10a
Mr. Is <isaac.ardis@gmail.com>
2016-07-29 15:44:27 -0400

    New dir with git config finder script.

---
+++ bin/find-git-configs.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Use:
+#   $ find_git_configs examples-dir echo
+#   $ find_git_configs examples-dir cat
+# 	$ find_git_configs examples-dir "sed -i.bak s/:3000//g"
+
+find_git_configs() {
+	find $1 -path "*/.git/config" -exec $2 {} \;
+}
+# -->
+# /Users/ia/dev/_rabbitxo/frogMob/.git/config
+# /Users/ia/dev/_rabbitxo/frogMob-app-backup/.git/config
+# /Users/ia/dev/_rabbitxo/frogxo/.git/config
+# /Users/ia/dev/_rabbitxo/hot sec/.git/config
+# /Users/ia/dev/_rstacks/beta-llc/.git/config
+# /Users/ia/dev/acronymus/.git/config
+# /Users/ia/dev/anagramatizer/.git/config
+# /Users/ia/dev/areteh/.git/config
+# /Users/ia/dev/betta/.git/config
+# /Users/ia/dev/betta/scratch/angular-arrow/.git/config
+# /Users/ia/dev/CodeforBoston/bikeways4everybody/.git/config
+# /Users/ia/dev/CodeforBoston/bypath/.git/config
+# ...
