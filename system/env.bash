3ac9159ae70b63e959988f37097927f2e9587322
Mr. Is <isaac.ardis@gmail.com>
2016-07-29 22:42:15 -0400

    riding the emacs train

--- system/env.bash
+++ system/env.bash
@@ -3,6 +3,20 @@ export VISUAL=vim
 #export EDITOR='slime -w'
 export EDITOR="$VISUAL"
 
+# the emacs train...
+# start a daemon on launchctrl for fast startup??...
+# per HN folk @ ycombi
+# emacs -nw --daemon
+# then,
+# emacsclient -new
+alias e="emacsclient -t" # requires $1
+# alias vim="e"
+# alias vi="e"
+# alias v="e"
+# fires up a 'clean' (ignores init files...) terminal mode emacs instance.
+# useless for a guy like me
+# alias emacsbare="emacs -new -Q --eval \"(load-them 'monokai')\""
+alias eb="emacsbare"
 
 export RBGOGS_PATH=/Users/ia/rotblauer/rbgogs
 
