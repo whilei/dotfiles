bda1e8a28d82f5ea178ddafe9868a5f6f215db8c
Mr. Is <isaac.ardis@gmail.com>
2016-07-30 06:28:38 -0400

    :floppy_disk:

--- bashers/aka-meta.bash
+++ bashers/aka-meta.bash
@@ -1,7 +1,2 @@
 AKA=~/.dotfiles/bashers/aka.zsh
 alias aka=". ~/.dotfiles/bashers/aka.zsh" # Below will be tailed.
-
-# function dockerevalenv() { eval $(docker-machine env $1); }
-# function dockeropen() { open http://$(docker-machine ip $1):$2; }
-
-export SPARKY_SLACK_API_TOKEN=xoxb-63382157255-PICZtqrlOPVHkrPGTFA2wT8Q
