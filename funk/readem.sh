2dcbf1bbb9b30e3f5e9a702c9bd85243ab993f6a
Mr. Is <isaac.ardis@gmail.com>
2016-07-30 06:34:07 -0400

    change var to makes sense

--- funk/readem.sh
+++ funk/readem.sh
@@ -2,75 +2,75 @@
 
 # run it from ~/.dotfiles/
 
-readempath="$HOME"/.dotfiles/TOC.md
+tocpath="$HOME"/.dotfiles/TOC.md
 
-echo "" > $readempath # overwrites the previous README
+echo "" > $tocpath # overwrites the previous README
 
 echo "
 ## Cloc
-> \`cloc . >> $readempath\`
-" >> $readempath
-echo "\`\`\`" >> $readempath
-cloc . >> $readempath
-echo "\`\`\`" >> $readempath
+> \`cloc . >> $tocpath\`
+" >> $tocpath
+echo "\`\`\`" >> $tocpath
+cloc . >> $tocpath
+echo "\`\`\`" >> $tocpath
 echo
 
 
 echo "
 ## Table of contents
-> \`tree >> $readempath\`
-" >> $readempath
-echo "\`\`\`" >> $readempath
-tree >> $readempath # --dirsfirst -alLhtDFC 4 -I .git
-echo "\`\`\`" >> $readempath
+> \`tree >> $tocpath\`
+" >> $tocpath
+echo "\`\`\`" >> $tocpath
+tree >> $tocpath # --dirsfirst -alLhtDFC 4 -I .git
+echo "\`\`\`" >> $tocpath
 echo
 
 
 echo "
 ## ... and the 5 most recently modified
-> gfind . -not -path \"*.git*\" -type f -printf \"%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\\n\" | sort | cut -f 2- -d ' ' | tail -5	>> $readempath
-" >> $readempath
-echo "\`\`\`" >> $readempath
-gfind . -not -path "*.git*" -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort | cut -f 2- -d ' ' | tail -5	>> $readempath
-echo "\`\`\`" >> $readempath
+> gfind . -not -path \"*.git*\" -type f -printf \"%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\\n\" | sort | cut -f 2- -d ' ' | tail -5	>> $tocpath
+" >> $tocpath
+echo "\`\`\`" >> $tocpath
+gfind . -not -path "*.git*" -type f -printf "%-.22T+ %M %n %-8u %-8g %8s %Tx %.8TX %p\n" | sort | cut -f 2- -d ' ' | tail -5	>> $tocpath
+echo "\`\`\`" >> $tocpath
 echo
 
 
 echo "
 ## Brewed dependencies
 > \`$ brew leaves >> README.md \`
-" >> $readempath
-echo "\`\`\`" >> $readempath
-brew leaves >> $readempath
-echo "\`\`\`" >> $readempath
+" >> $tocpath
+echo "\`\`\`" >> $tocpath
+brew leaves >> $tocpath
+echo "\`\`\`" >> $tocpath
 echo
 
 
 echo "
 ## Ruby gems
 > \`env GEM_PATH=$GEM_HOME gem list\`
-" >> $readempath
-echo "\`\`\`" >> $readempath
-env GEM_PATH=$GEM_HOME gem list >> $readempath
-echo "\`\`\`" >> $readempath
+" >> $tocpath
+echo "\`\`\`" >> $tocpath
+env GEM_PATH=$GEM_HOME gem list >> $tocpath
+echo "\`\`\`" >> $tocpath
 
 
 echo "
 ## Go src packages
-> \`pushd $GOPATH/src/; tree -L 3 >> $readempath; popd;\`
-" >> $readempath
-echo "\`\`\`" >> $readempath
-pushd $GOPATH/src/; tree -L 3 >> $readempath; popd;
-echo "\`\`\`" >> $readempath
+> \`pushd $GOPATH/src/; tree -L 3 >> $tocpath; popd;\`
+" >> $tocpath
+echo "\`\`\`" >> $tocpath
+pushd $GOPATH/src/; tree -L 3 >> $tocpath; popd;
+echo "\`\`\`" >> $tocpath
 
 
 echo "
 ## NPM modules
-> \`pushd ~/.node/lib/; npm ls >> $readempath; popd;\`
-" >> $readempath
-echo "\`\`\`" >> $readempath
-pushd ~/.node/lib/; npm ls >> $readempath; popd;
-echo "\`\`\`" >> $readempath
+> \`pushd ~/.node/lib/; npm ls >> $tocpath; popd;\`
+" >> $tocpath
+echo "\`\`\`" >> $tocpath
+pushd ~/.node/lib/; npm ls >> $tocpath; popd;
+echo "\`\`\`" >> $tocpath
 
 
 
