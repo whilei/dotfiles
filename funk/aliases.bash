93f78536075bf4d279daf1aa100fc40e770f8000
Mr. Is <isaac.ardis@gmail.com>
2017-06-06 14:53:27 -0500

    :floppy_disk:

--- funk/aliases.bash
+++ funk/aliases.bash
@@ -2,11 +2,9 @@
 alias busy="cat /dev/urandom | hexdump -C | grep 'ca fe'"
 
 alias paper="bash $HOME/.dotfiles/funk/paper.sh" # explicit bash command here seems to fix ? some getopts issues
-alias build_readem="bash $HOME/.dotfiles/funk/readem.sh"
 
 # for app store screenshot making
 alias screenShotter="bash $HOME/.dotfiles/scripts/screenshotter.sh"
 
+alias comingover="echo https://www.youtube.com/watch?v=IX2EGn7NQI4 | pbcopy"
 
-# Lists the ten most used commands.
-alias history_topten="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
--- go/aliases.bash
+++ go/aliases.bash
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
+alias gotv="go test -v"
+gotvf() {
+	args="$*"
+	echo "Testing: $args"
 
+	out=$(go test -v "$args" )
+
+	fails=$(grep ---\ FAIL: <<< "$out")
+	passes=$(grep ---\ PASS: <<< "$out")
+	failsN=$(wc -l <<< "$fails")
+	passesN=$(wc -l <<< "$passes")
+
+	echo "Fail: $failsN, Pass: $passesN"
+	echo "$fails"
+
+	unset args
+	unset out
+	unset fails
+	unset passes
+	unset failsN
+	unset passesN
+}
