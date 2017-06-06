e6f79dca7d606934277e26e6b3baf809656bb8be
Mr. Is <isaac.ardis@gmail.com>
2017-06-06 08:17:40 -0500

    gtw v1

---
+++ funk/gtw.bash
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+gtw() {
+	open -a Safari
+	open -a Slack
+	open -a Gogland
+	open -a "Sublime Text"
+	say "Enough words have been exchanged. Now let me see some deeds. What is not done today will not be done tomorrow."
+}
