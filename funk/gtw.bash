aa25b040cd3341f34366cd8364997876b53c80af
ia <isaac.ardis@gmail.com>
2017-07-07 07:27:49 -0500

    early access

--- funk/gtw.bash
+++ funk/gtw.bash
@@ -4,6 +4,6 @@ gtw() {
 	open -a 'Google Chrome Canary' http://goggable.areteh.co:3000
 	open -a 'Google Chrome Canary' https://github.com
 	open -a Slack
-	open -a Gogland
+	open -a Gogland-EAP
 	say "Enough words have been exchanged. Now let me see some deeds. What is not done today will not be done tomorrow."
 }
