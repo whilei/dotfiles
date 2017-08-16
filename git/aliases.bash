28e7ea2cb43efcee1c8abbe65d9b16ef44aa792c
ia <isaac.ardis@gmail.com>
2017-08-16 13:39:17 -0500

    fuzzy branch delete. :awesome:

--- git/aliases.bash
+++ git/aliases.bash
@@ -143,4 +143,19 @@ cof() {
 alias gcof="cof"
 alias cofz="cof"
 alias coz="cof"
+alias fzco="cof"
+alias fzgco="cof"
+
+bdf() {
+	local branches branch
+	branches=$(git branch -a) &&
+	branch=$(echo "$branches" | fzf +s +m -e) &&
+	git branch -d $(echo "$branch" | sed "s:.* remotes/origin/::" | sed "s:.* ::")
+}
+
+alias gbdf="bdf"
+alias bdfz="bdf"
+alias fzgbd="bdf"
+alias fgbd="bdf"
+alias fzgbd="bdf"
 
