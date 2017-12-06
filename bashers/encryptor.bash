8d25e000514e56510599ee744e810376ef85bedf
ia <isaac.ardis@gmail.com>
2017-12-06 11:01:35 -0600

    add crypter aliases, add sha and md5 convenience funcs

--- bashers/encryptor.bash
+++ bashers/encryptor.bash
@@ -9,6 +9,7 @@ encrypt(){
 		openssl enc -aes-256-cbc -salt -in "$1" -out "$2"
 	fi
 }
+alias enc="encrypt"
 
 decrypt(){
 	if [[ $# -ne 2 ]]; then
@@ -19,3 +20,24 @@ decrypt(){
 		openssl enc -d -aes-256-cbc -in "$1" -out "$2"
 	fi
 }
+alias dec="decrypt"
+
+sha(){
+	if [[ $# -lt 1 ]]; then
+		echo "Use"
+		echo "$ sha256 file"
+	else
+		openssl sha -sha256 $1
+	fi
+}
+alias sha256="sha"
+
+md5(){
+	if [[ $# -lt 1 ]]; then
+		echo "Use"
+		echo "$ md5 file"
+	else
+		openssl md5 $1
+	fi
+}
+alias md5sum="md5"
