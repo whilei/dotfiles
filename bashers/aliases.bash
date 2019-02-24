d698cd37eba40cbaff14efcb8f28f86319012b7e
ia <isaac.ardis@gmail.com>
2019-02-24 05:54:45 -0600

    lastt modified file+dir aliases

--- bashers/aliases.bash
+++ bashers/aliases.bash
@@ -35,3 +35,6 @@ len() {
 	echo -n $size
 }
 
+# last modified file in cwd
+alias lmf="find . -maxdepth 1 -type f -printf '%T@ %Tc %p\n' | sort -n | tail -1 | cut -d' ' -f9"
+alias lmd="find . -maxdepth 1 -type d -not -name '.git' -printf '%T@ %Tc %p\n' | sort -n | tail -1 | cut -d' ' -f9"
