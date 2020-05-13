5b0afadb69f8b824021b2d58b2b3b882b779312c
ia <isaac.ardis@gmail.com>
2020-05-13 16:09:49 -0500

    :floppy_disk:

--- funk/dev-whiteboard.bash
+++ funk/dev-whiteboard.bash
@@ -1,4 +1,4 @@
 # Create whiteboard file for any dev/ project.
-whiteboard() {
-	command vim ~/dev/$1/scratch/whiteboard/`date_stamp`.md
-}
+# whiteboard() {
+# 	command vim ~/dev/$1/scratch/whiteboard/`date_stamp`.md
+# }
