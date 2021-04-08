6722f7952b9dab374610dc8e79c08481c7f98de8
ia <isaac.ardis@gmail.com>
2021-04-08 07:40:50 -0500

    set LB as last branch value, akin to cd -

--- git/git-prompt-init.bash
+++ git/git-prompt-init.bash
@@ -102,3 +102,33 @@ alias ps1_sml="ps1_sm"
 alias ps1_short="ps1_sm"
 alias ps1_norm="ps1_lg"
 
+# I want a value in my shell for git branch names that works like cd's '-' value.
+
+# LB 'Last Branch'
+export LB=""
+export __lb_current_branch=""
+
+__set_last_branch(){
+  local b="";
+  b=$(2> /dev/null git symbolic-ref --short HEAD)
+
+  if [ -z "$b" ]; then return; fi
+  
+  if [ "$__lb_current_branch" != "$b" ]; then
+    if [ "$__lb_current_branch" = "" ]; then
+      # Initialize
+      export LB="$b"
+    else
+      # Update
+      export LB="$__lb_current_branch"
+    fi
+  fi
+
+  export __lb_current_branch="$b"
+}
+export -f __set_last_branch
+export PROMPT_COMMAND="$PROMPT_COMMAND __set_last_branch;"
+
+
+
+
