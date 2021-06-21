1af4e1a9492a7cec1478c5daab9c0a648da1b0fd
ia <isaac.ardis@gmail.com>
2021-06-21 08:55:17 -0500

    rm etc/, fix ltf
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- etc/emerald.bash
+++
@@ -1,5 +0,0 @@
-# Default chain value emerald-cli
-export EMERALD_CHAIN=mainnet
-
-# EMERALD_GAS=0x2100
-# EMERALD_GAS_PRICE=0x21
--- etc/keybase.bash
+++
@@ -1,5 +0,0 @@
-alias kc="keybase chat"
-alias kcl="keybase chat list"
-alias kcr="keybase chat read"
-alias kcs="keybase chat send"
-
