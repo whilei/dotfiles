3fd47e983ff87982d845008de836e6236490ec74
ia <isaac.ardis@gmail.com>
2021-09-16 10:00:36 -0700

    :floppy_disk:
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- etc/aliases.bash
+++ etc/aliases.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-alias francais="vlc '/home/ia/SAMSUNG_T5/Music/Pimsleur-French/Pimsleur - French II'"
+alias francais="echo '/home/ia/SAMSUNG_T5/Music/Pimsleur-French/Pimsleur-French-III'"
